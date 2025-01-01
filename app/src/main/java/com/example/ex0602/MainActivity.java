becefc88b16507420e592216ded48da08780f1e0
av2215 <av2215@bs.amalnet.k12.il>
2025-01-02 00:39:43 +0200

    hiii

--- app/src/main/java/com/example/ex0602/MainActivity.java
+++ app/src/main/java/com/example/ex0602/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ex0602;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +12,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button shaked;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        shaked=(Button) findViewById(R.id.albert);
+    }
+
+    public void one(View view) {
+        shaked.setText("Oh, yea, I’ve been clicked!");
     }
 }
